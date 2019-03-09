2f43b80e2a24c022343c33996bd21aff5f7f8b13
Etienne Dilocker <etienne@dilocker.de>
2019-03-09 13:47:37 +0100

    gh-1: add 'IsWordPresent' rpc method
    currently stubbed out in the server api, will be filled with actual
    content soon :)

--- contextionary/contextionary.pb.go
+++ contextionary/contextionary.pb.go
@@ -53,6 +53,84 @@ func (m *Void) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_Void proto.InternalMessageInfo
 
+type Word struct {
+	Word                 string   `protobuf:"bytes,1,opt,name=word,proto3" json:"word,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *Word) Reset()         { *m = Word{} }
+func (m *Word) String() string { return proto.CompactTextString(m) }
+func (*Word) ProtoMessage()    {}
+func (*Word) Descriptor() ([]byte, []int) {
+	return fileDescriptor_e6af9fd695f521f0, []int{1}
+}
+
+func (m *Word) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_Word.Unmarshal(m, b)
+}
+func (m *Word) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_Word.Marshal(b, m, deterministic)
+}
+func (m *Word) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_Word.Merge(m, src)
+}
+func (m *Word) XXX_Size() int {
+	return xxx_messageInfo_Word.Size(m)
+}
+func (m *Word) XXX_DiscardUnknown() {
+	xxx_messageInfo_Word.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_Word proto.InternalMessageInfo
+
+func (m *Word) GetWord() string {
+	if m != nil {
+		return m.Word
+	}
+	return ""
+}
+
+type WordPresent struct {
+	Present              bool     `protobuf:"varint,1,opt,name=present,proto3" json:"present,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *WordPresent) Reset()         { *m = WordPresent{} }
+func (m *WordPresent) String() string { return proto.CompactTextString(m) }
+func (*WordPresent) ProtoMessage()    {}
+func (*WordPresent) Descriptor() ([]byte, []int) {
+	return fileDescriptor_e6af9fd695f521f0, []int{2}
+}
+
+func (m *WordPresent) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_WordPresent.Unmarshal(m, b)
+}
+func (m *WordPresent) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_WordPresent.Marshal(b, m, deterministic)
+}
+func (m *WordPresent) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_WordPresent.Merge(m, src)
+}
+func (m *WordPresent) XXX_Size() int {
+	return xxx_messageInfo_WordPresent.Size(m)
+}
+func (m *WordPresent) XXX_DiscardUnknown() {
+	xxx_messageInfo_WordPresent.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_WordPresent proto.InternalMessageInfo
+
+func (m *WordPresent) GetPresent() bool {
+	if m != nil {
+		return m.Present
+	}
+	return false
+}
+
 type Message struct {
 	Message              string   `protobuf:"bytes,1,opt,name=message,proto3" json:"message,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
@@ -64,7 +142,7 @@ func (m *Message) Reset()         { *m = Message{} }
 func (m *Message) String() string { return proto.CompactTextString(m) }
 func (*Message) ProtoMessage()    {}
 func (*Message) Descriptor() ([]byte, []int) {
-	return fileDescriptor_e6af9fd695f521f0, []int{1}
+	return fileDescriptor_e6af9fd695f521f0, []int{3}
 }
 
 func (m *Message) XXX_Unmarshal(b []byte) error {
@@ -94,21 +172,27 @@ func (m *Message) GetMessage() string {
 
 func init() {
 	proto.RegisterType((*Void)(nil), "contextionary.Void")
+	proto.RegisterType((*Word)(nil), "contextionary.Word")
+	proto.RegisterType((*WordPresent)(nil), "contextionary.WordPresent")
 	proto.RegisterType((*Message)(nil), "contextionary.Message")
 }
 
 func init() { proto.RegisterFile("contextionary.proto", fileDescriptor_e6af9fd695f521f0) }
 
 var fileDescriptor_e6af9fd695f521f0 = []byte{
-	// 123 bytes of a gzipped FileDescriptorProto
+	// 189 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x12, 0x4e, 0xce, 0xcf, 0x2b,
 	0x49, 0xad, 0x28, 0xc9, 0xcc, 0xcf, 0x4b, 0x2c, 0xaa, 0xd4, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17,
-	0xe2, 0x45, 0x11, 0x54, 0x62, 0xe3, 0x62, 0x09, 0xcb, 0xcf, 0x4c, 0x51, 0x52, 0xe6, 0x62, 0xf7,
-	0x4d, 0x2d, 0x2e, 0x4e, 0x4c, 0x4f, 0x15, 0x92, 0xe0, 0x62, 0xcf, 0x85, 0x30, 0x25, 0x18, 0x15,
-	0x18, 0x35, 0x38, 0x83, 0x60, 0x5c, 0x23, 0x2f, 0x2e, 0x5e, 0x67, 0x64, 0xdd, 0x42, 0x96, 0x5c,
-	0x1c, 0xee, 0xa9, 0x25, 0x1e, 0xa9, 0x39, 0x39, 0xf9, 0x42, 0xc2, 0x7a, 0xa8, 0xd6, 0x81, 0x8c,
-	0x95, 0x12, 0x43, 0x13, 0x84, 0xda, 0xa1, 0xc4, 0x90, 0xc4, 0x06, 0x76, 0x8e, 0x31, 0x20, 0x00,
-	0x00, 0xff, 0xff, 0x99, 0x89, 0x4c, 0x5f, 0xa5, 0x00, 0x00, 0x00,
+	0xe2, 0x45, 0x11, 0x54, 0x62, 0xe3, 0x62, 0x09, 0xcb, 0xcf, 0x4c, 0x51, 0x92, 0xe2, 0x62, 0x09,
+	0xcf, 0x2f, 0x4a, 0x11, 0x12, 0xe2, 0x62, 0x29, 0xcf, 0x2f, 0x4a, 0x91, 0x60, 0x54, 0x60, 0xd4,
+	0xe0, 0x0c, 0x02, 0xb3, 0x95, 0xd4, 0xb9, 0xb8, 0x41, 0x72, 0x01, 0x45, 0xa9, 0xc5, 0xa9, 0x79,
+	0x25, 0x42, 0x12, 0x5c, 0xec, 0x05, 0x10, 0x26, 0x58, 0x15, 0x47, 0x10, 0x8c, 0xab, 0xa4, 0xcc,
+	0xc5, 0xee, 0x9b, 0x5a, 0x5c, 0x9c, 0x98, 0x9e, 0x0a, 0x52, 0x94, 0x0b, 0x61, 0x42, 0x8d, 0x82,
+	0x71, 0x8d, 0xfa, 0x18, 0xb9, 0x78, 0x9d, 0x91, 0xdd, 0x20, 0x64, 0xc9, 0xc5, 0xe1, 0x9e, 0x5a,
+	0xe2, 0x91, 0x9a, 0x93, 0x93, 0x2f, 0x24, 0xac, 0x87, 0xea, 0x68, 0x90, 0xe3, 0xa4, 0xc4, 0xd0,
+	0x04, 0xa1, 0x96, 0x28, 0x31, 0x08, 0x39, 0x71, 0xf1, 0x7a, 0x16, 0x23, 0x3b, 0x0e, 0x5d, 0x3f,
+	0x48, 0x4e, 0x4a, 0x0a, 0x8b, 0x20, 0x54, 0x83, 0x12, 0x43, 0x12, 0x1b, 0x38, 0x60, 0x8c, 0x01,
+	0x01, 0x00, 0x00, 0xff, 0xff, 0x58, 0x1f, 0x51, 0x2d, 0x2f, 0x01, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -124,6 +208,7 @@ const _ = grpc.SupportPackageIsVersion4
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type ContextionaryClient interface {
 	GetHello(ctx context.Context, in *Void, opts ...grpc.CallOption) (*Message, error)
+	IsWordPresent(ctx context.Context, in *Word, opts ...grpc.CallOption) (*WordPresent, error)
 }
 
 type contextionaryClient struct {
@@ -143,9 +228,19 @@ func (c *contextionaryClient) GetHello(ctx context.Context, in *Void, opts ...gr
 	return out, nil
 }
 
+func (c *contextionaryClient) IsWordPresent(ctx context.Context, in *Word, opts ...grpc.CallOption) (*WordPresent, error) {
+	out := new(WordPresent)
+	err := c.cc.Invoke(ctx, "/contextionary.Contextionary/IsWordPresent", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // ContextionaryServer is the server API for Contextionary service.
 type ContextionaryServer interface {
 	GetHello(context.Context, *Void) (*Message, error)
+	IsWordPresent(context.Context, *Word) (*WordPresent, error)
 }
 
 func RegisterContextionaryServer(s *grpc.Server, srv ContextionaryServer) {
@@ -170,6 +265,24 @@ func _Contextionary_GetHello_Handler(srv interface{}, ctx context.Context, dec f
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Contextionary_IsWordPresent_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(Word)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ContextionaryServer).IsWordPresent(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/contextionary.Contextionary/IsWordPresent",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ContextionaryServer).IsWordPresent(ctx, req.(*Word))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _Contextionary_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "contextionary.Contextionary",
 	HandlerType: (*ContextionaryServer)(nil),
@@ -177,6 +290,10 @@ var _Contextionary_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "GetHello",
 			Handler:    _Contextionary_GetHello_Handler,
+		},
+		{
+			MethodName: "IsWordPresent",
+			Handler:    _Contextionary_IsWordPresent_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
--- server/api.go
+++ server/api.go
@@ -7,8 +7,11 @@ import (
 )
 
 func (s *server) GetHello(ctx context.Context, in *pb.Void) (*pb.Message, error) {
-
 	return &pb.Message{
 		Message: "hello world",
 	}, nil
+}
+
+func (s *server) IsWordPresent(ctx context.Context, word *pb.Word) (*pb.WordPresent, error) {
+	return &pb.WordPresent{Present: false}, nil
 }
