7806a95bc91d69a91526f2154e02d58e8661b9ed
Etienne Dilocker <etienne@dilocker.de>
2019-05-27 14:11:43 +0200

    gh-1: improve build steps for minimal version

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-docker build -f Dockerfile.dev -t "$DOCKER_ORG/$DOCKER_REPO:$TAG" .
+docker build -f Dockerfile.minimal -t "$DOCKER_ORG/$DOCKER_REPO:$VERSION-minimal" .
--- docker_push.sh
+++ docker_push.sh
@@ -4,5 +4,5 @@ set -e
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 set -x
-docker push "$DOCKER_ORG/$DOCKER_REPO:$TAG"
+docker push "$DOCKER_ORG/$DOCKER_REPO:$VERSION-minimal"
 
