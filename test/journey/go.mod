bc38c631b813aafe8e7fb66da07a73d05c9506b6
Etienne Dilocker <etienne@dilocker.de>
2019-05-29 11:06:25 +0200

    gh-1: add journey tests

--- test/journey/go.mod
+++ test/journey/go.mod
@@ -1,3 +1,9 @@
 module github.com/semi-technologies/contextionary/test/journey
 
 go 1.12
+
+require (
+	github.com/semi-technologies/contextionary v0.0.0-20190529072917-998b87112585
+	github.com/stretchr/testify v1.3.0
+	google.golang.org/grpc v1.21.0
+)
--- test/journey/journey_test.go
+++ test/journey/journey_test.go
@@ -1,7 +1,85 @@
 package main
 
-import "testing"
+import (
+	"context"
+	"testing"
 
-func TestHelloWorld(t *testing.T) {
-	t.Fatal("not implemented")
+	pb "github.com/semi-technologies/contextionary/contextionary"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+)
+
+func Test_Contextionary_Journey(t *testing.T) {
+	// minimal
+	connMinimal, err := grpc.Dial("minimal:9999", grpc.WithInsecure())
+	if err != nil {
+		t.Fatalf("couldn't connect to minimal c11y: %s", err)
+	}
+	defer connMinimal.Close()
+
+	connFull, err := grpc.Dial("full:9999", grpc.WithInsecure())
+	if err != nil {
+		t.Fatalf("couldn't connect to minimal c11y: %s", err)
+	}
+	defer connFull.Close()
+
+	clientMinimal := pb.NewContextionaryClient(connMinimal)
+	clientFull := pb.NewContextionaryClient(connFull)
+
+	t.Run("the minimal contextionary", func(t *testing.T) {
+		client := clientMinimal
+
+		t.Run("testing words present", func(t *testing.T) {
+			words := []string{"car", "engine", "automobile", "name"}
+
+			for _, word := range words {
+				t.Run(word, func(t *testing.T) {
+					res, err := client.IsWordPresent(context.Background(), &pb.Word{Word: word})
+					require.Nil(t, err)
+					assert.Equal(t, true, res.Present)
+				})
+			}
+		})
+
+		t.Run("testing stopwords", func(t *testing.T) {
+			words := []string{"of", "the"}
+
+			for _, word := range words {
+				t.Run(word, func(t *testing.T) {
+					res, err := client.IsWordStopword(context.Background(), &pb.Word{Word: word})
+					require.Nil(t, err)
+					assert.Equal(t, true, res.Stopword)
+				})
+			}
+		})
+	})
+
+	t.Run("the full contextionary", func(t *testing.T) {
+		client := clientFull
+
+		t.Run("testing words present", func(t *testing.T) {
+			words := []string{"car", "engine", "automobile", "influenza", "brexit", "condenser", "name"}
+
+			for _, word := range words {
+				t.Run(word, func(t *testing.T) {
+					res, err := client.IsWordPresent(context.Background(), &pb.Word{Word: word})
+					require.Nil(t, err)
+					assert.Equal(t, true, res.Present)
+				})
+			}
+		})
+
+		t.Run("testing stopwords", func(t *testing.T) {
+			words := []string{"of", "the", "a", "ah", "almost", "always", "also", "as"}
+
+			for _, word := range words {
+				t.Run(word, func(t *testing.T) {
+					res, err := client.IsWordStopword(context.Background(), &pb.Word{Word: word})
+					require.Nil(t, err)
+					assert.Equal(t, true, res.Stopword)
+				})
+			}
+		})
+	})
 }
