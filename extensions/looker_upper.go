89934b4a2d3808b7766c288edc0ab4349b876ed8
Etienne Dilocker <etienne@dilocker.de>
2019-10-29 14:35:15 +0100

    gh-14 add storer with vectorization and validator

--- extensions/looker_upper.go
+++ extensions/looker_upper.go
@@ -5,18 +5,18 @@ import (
 )
 
 type LookerUpper struct {
-	repo Repo
+	repo RetrieverRepo
 	sync.Mutex
 	db map[string]Extension
 }
 
-type Repo interface {
+type RetrieverRepo interface {
 	// WatchAll must send an immediate response after opening (for
 	// initializiation), then send another response whenver the db has changed
 	WatchAll() chan WatchResponse
 }
 
-func NewLookerUpper(repo Repo) *LookerUpper {
+func NewLookerUpper(repo RetrieverRepo) *LookerUpper {
 	lu := &LookerUpper{
 		repo: repo,
 		db:   map[string]Extension{},
---
+++ extensions/storer.go
@@ -0,0 +1,87 @@
+package extensions
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"unicode"
+
+	core "github.com/semi-technologies/contextionary/contextionary/core"
+)
+
+type Vectorizer interface {
+	Corpi(corpi []string) (*core.Vector, error)
+}
+
+type StorerRepo interface {
+	Put(ctx context.Context, ext Extension) error
+}
+
+type Storer struct {
+	vectorizer Vectorizer
+	repo       StorerRepo
+}
+
+func NewStorer(vectorizer Vectorizer, repo StorerRepo) *Storer {
+	return &Storer{vectorizer, repo}
+}
+
+func (s *Storer) Put(ctx context.Context, concept string, input ExtensionInput) error {
+	err := s.validate(concept, input)
+	if err != nil {
+		return fmt.Errorf("invalid extension: %v", err)
+	}
+
+	vector, err := s.vectorizer.Corpi([]string{input.Definition})
+	if err != nil {
+		return fmt.Errorf("vectorize definition: %v", err)
+	}
+
+	concept = s.compound(concept)
+
+	ext := Extension{
+		Concept:    concept,
+		Input:      input,
+		Vector:     *vector, // vector is safe to dereference as vectorizer will always return a non-nil vector if error is nil
+		Occurrence: 1000,    // TODO: Improve!
+	}
+
+	err = s.repo.Put(ctx, ext)
+	if err != nil {
+		return fmt.Errorf("store extension: %v", err)
+	}
+
+	return nil
+}
+
+func (s *Storer) compound(inp string) string {
+	parts := strings.Split(inp, " ")
+	return strings.Join(parts, "_")
+}
+
+func (s *Storer) validate(concept string, input ExtensionInput) error {
+	if len(concept) < 2 {
+		return fmt.Errorf("concept must have at least two characters")
+	}
+
+	for _, r := range concept {
+		if !unicode.IsLower(r) && !unicode.IsSpace(r) && !unicode.IsNumber(r) {
+			return fmt.Errorf("concept must be made up of all lowercase letters and/or numbers, " +
+				"for custom compund words use spaces, e.g. 'flux capacitor'")
+		}
+	}
+
+	if len(input.Definition) == 0 {
+		return fmt.Errorf("definition cannot be empty")
+	}
+
+	if input.Weight > 1 || input.Weight < 0 {
+		return fmt.Errorf("weight must be between 0 and 1")
+	}
+
+	if input.Weight < 1 {
+		return fmt.Errorf("weights below 1 (extending an existing concept) not supported yet - coming soon")
+	}
+
+	return nil
+}
